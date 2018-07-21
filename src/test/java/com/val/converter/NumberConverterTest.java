2655d8cfdaf11d09422ca696b624647dad1f0851
iggy <elizaveta@MacBook-Pro-li.local>
2018-07-21 03:08:52 +0300

    NumberConverterDataDrivenTest removed

--- src/test/java/com/val/converter/NumberConverterTest.java
+++ src/test/java/com/val/converter/NumberConverterTest.java
@@ -1,52 +1,47 @@
 package com.val.converter;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NumberConverterTest {
 
-    private final NumberConverter formatter = new NumberConverter();
+    private static final NumberConverter instance = new NumberConverter();
 
     @Test(expected = NumberFormatException.class)
-    public void argIsNotNumber_throwsException(){
-        formatter.asWords("123dfr");
+    public void testAsWordsNotANumberThenException(){
+        instance.asWords("123dfr");
     }
 
     @Test
-    public void argIsOk_noException(){
-        formatter.asWords("9876123456");
+    public void testAsWordsStringType(){
+        String arg = "2002";
+        assertEquals("две тысячи два", instance.asWords(arg));
     }
 
     @Test
-    public void stringArgument_returnsNumberInWordRepresentation(){
-        assertEquals("ноль", formatter.asWords("0"));
+    public void testAsWordsLongType(){
+        Long arg = 2002L;
+        assertEquals("две тысячи два", instance.asWords(arg));
     }
 
     @Test
-    public void stringArgument_returnsNumberInWordRepresentation_2(){
-        assertEquals("две тысячи два", formatter.asWords("2002"));
+    public void testAsWordsNegative(){
+        Long arg = -33L;
+        assertEquals("минус тридцать три", instance.asWords(arg));
     }
 
     @Test
-    public void stringArgument_returnsNumberInWordRepresentation_3(){
-        assertEquals("минус три", formatter.asWords("-3"));
+    public void testAsWordsDataDriven(){
+
+        TestDataFileDao testDataFileDao = new TestDataFileDao();
+        TestDataFileEntry pair;
+
+        while (testDataFileDao.hasNext()){
+            pair = testDataFileDao.next();
+            Assert.assertEquals(pair.expectedValue, instance.asWords(pair.argumentValue));
+        }
+
     }
-
-    @Test
-    public void longArgument_returnsNumberInWordRepresentation_1(){
-        assertEquals("ноль", formatter.asWords(0));
-    }
-
-    @Test
-    public void longArgument_returnsNumberInWordRepresentation__2(){
-        assertEquals("две тысячи два", formatter.asWords(2002));
-    }
-
-    @Test
-    public void longArgument_returnsNumberInWordRepresentation_3(){
-        assertEquals("минус три", formatter.asWords(-3));
-    }
-
-
 }
